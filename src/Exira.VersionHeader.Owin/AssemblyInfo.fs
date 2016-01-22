6cbee33a71137d21a8da99a3f87913858cabb7f8
Team City <info@exira.com>
2016-01-22 20:01:08 +0000

    Bump version to 0.3.15

--- src/Exira.VersionHeader.Owin/AssemblyInfo.fs
+++ src/Exira.VersionHeader.Owin/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.VersionHeader.Owin")>]
 [<assembly: AssemblyProductAttribute("Exira.VersionHeader.Owin")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.VersionHeader.Owin is an OWIN middleware to add assembly version information to HTTP headers")>]
-[<assembly: AssemblyVersionAttribute("0.2.14")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.14")>]
-[<assembly: AssemblyMetadataAttribute("githash","87a827a999cf1d5e800ec6e8c3868c1a32340635")>]
+[<assembly: AssemblyVersionAttribute("0.3.15")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.15")>]
+[<assembly: AssemblyMetadataAttribute("githash","4c86051c1d5241c1b374007f76efabd5aaf644e2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.14"
+    let [<Literal>] Version = "0.3.15"
